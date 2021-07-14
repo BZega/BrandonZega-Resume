1ba5800ad929b01b2690402f0fa6be70f6d8def6
Brandon Zega <brandon_zega@yahoo.com>
2021-07-13 20:22:17 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# May2021-Project
---
+++ js/scripts.js
@@ -0,0 +1,21 @@
+function validation(theForm){
+    var name = document.getElementById("name").value;
+    var email = document.getElementById("mail").value;
+    var feedback = document.getElementById("feedback").value;
+        if( name ===''){
+            alert("Please fill in your Name!");
+            return false;
+        }
+        else if( email ===''){
+            alert("Please fill in your E-mail!");
+            return false;
+        }
+        else if( feedback ===''){
+            alert("Please fill in your Message!");
+            return false;
+        }
+        else{
+            alert('Thank you, ' + name + ', your message: "' + feedback + '" has been sent to Brandon Zega! You will receive a reply shortly. Thank you, and have a great day!')
+            return true;
+        }
+}
